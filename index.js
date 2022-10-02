9ca57e2a596aaca068631cbc14d22894833f6460
Ayush <aayush199813@gmail.com>
2022-10-02 12:59:48 +0530

    file setup

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-const express = require("express");
+const path = require('path');
+const PORT = 3000;
+// using express as a server
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+// initializing express
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// error controller
+const errorController = require('./controllers/error');
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // design file
 app.use(express.static("public"));
-app.set("view engine", "ejs");
 
-// routers
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// static file
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// import routes
+const index = require('./routes/index');
+
+
+// body parsering
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+// accessing uploaded file from uploads
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+
+// use Routes
+app.use(index);
+app.use(errorController.get404);
 
 // server listening
 app.listen(PORT, () => {
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const home = require('../controllers/homeController');
+
+
+router.get('/', home.getHome);
+
+
+
+module.exports = router;
